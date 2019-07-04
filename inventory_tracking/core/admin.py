5f57f172bb1b15e302345c3e3703ec3c58d91d5c
onurtacc <onur.tac@orgino.com.tr>
2019-07-04 23:06:19 +0300

    add models

--- inventory_tracking/core/admin.py
+++ inventory_tracking/core/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Building, Apartment, Room, Furniture
 
-# Register your models here.
+
+class BuildingAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'no')
+    list_display = ('name', 'no')
+
+
+admin.site.register(Building, BuildingAdmin)
+
+
+class Rooms(admin.StackedInline):
+    model = Room
+    extra = 0
+
+
+class ApartmentAdmin(admin.ModelAdmin):
+    search_fields = ('building', 'apartment_no', 'floor', 'square_meter')
+    list_display = ('apartment_no', 'building', 'floor', 'square_meter')
+    list_filter = ["building"]
+
+    inlines = [
+        Rooms
+    ]
+
+
+admin.site.register(Apartment, ApartmentAdmin)
+
+
+class FurnitureAdmin(admin.ModelAdmin):
+    search_fields = ('room', 'name')
+    list_display = ('name', 'price', 'room')
+    list_filter = ["room"]
+
+
+admin.site.register(Furniture, FurnitureAdmin)
--- inventory_tracking/core/models.py
+++ inventory_tracking/core/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from inventory_tracking.common.timetrackedmodel import TimeTrackedModel
 
-# Create your models here.
+
+class Building(TimeTrackedModel):
+    name = models.CharField(max_length=255)
+    no = models.CharField(max_length=255)
+    address = models.TextField()
+
+    class Meta:
+        verbose_name = "Bina"
+        verbose_name_plural = "Binalar"
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.name
+
+
+class Apartment(TimeTrackedModel):
+    building = models.ForeignKey(Building, related_name="apartments", on_delete=models.CASCADE)
+    apartment_no = models.IntegerField()
+    floor = models.IntegerField()
+    square_meter = models.IntegerField()
+
+    class Meta:
+        unique_together = ("building", "apartment_no")
+        verbose_name = "Daire"
+        verbose_name_plural = "Daireler"
+        ordering = ['apartment_no']
+
+    def __str__(self):
+        return self.building.name + " - " + str(self.apartment_no)
+
+
+class Room(TimeTrackedModel):
+    apartment = models.ForeignKey(Apartment, related_name="rooms", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, help_text="for example: kitchen, living room etc.")
+
+    class Meta:
+        verbose_name = "Oda"
+        verbose_name_plural = "Odalar"
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.apartment.__str__() + " - " + self.name
+
+
+class Furniture(TimeTrackedModel):
+    room = models.ForeignKey(Room, related_name="furnishings", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    price = models.IntegerField(verbose_name="Price (₺)")
+
+    class Meta:
+        verbose_name = "Eşya"
+        verbose_name_plural = "Eşyalar"
+        ordering = ['created_at']
+
+    def __str__(self):
+        return self.name
